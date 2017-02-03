fd5993aad030fda3fb1eb30df550ab2da9e4e20c
Kate Zasulevich <katejidkova@gmail.com>
2017-02-04 02:05:16 +0300

    add all;

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,18 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
-
+        this.minNumber = min+1;
+        this.maxNumber = max-1;
     }
-
     guess() {
-
+        this.currentPosition = Math.round((this.minNumber + this.maxNumber) / 2);
+        return this.currentPosition;
+        
     }
-
     lower() {
-
+        this.maxNumber = this.currentPosition - 1;
     }
-
     greater() {
-
+        this.minNumber = this.currentPosition + 1;
     }
 }
-
 module.exports = GuessingGame;
